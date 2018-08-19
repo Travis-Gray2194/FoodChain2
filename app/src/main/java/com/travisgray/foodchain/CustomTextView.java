aba1b68ac50e1961d70333d179ddd8b62f3ac12e
Travis Gray <travisogray1@gmail.com>
2018-08-19 09:32:53 -0400

    Initial commit
    Sign up page start with content

---
+++ app/src/main/java/com/travisgray/foodchain/CustomTextView.java
@@ -0,0 +1,24 @@
+package com.travisgray.foodchain;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.TextViewCompat;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by travi on 7/30/2018.
+ */
+//
+//public class CustomTextView extends TextView {
+//
+//    public CustomTextView(Context context, AttributeSet attrs) {
+//        super(context, attrs);
+//        this.setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/CostaRica.ttf"));
+//    }
+//
+//
+//
+//
+//}
---
+++ app/src/main/java/com/travisgray/foodchain/MainActivity.java
@@ -0,0 +1,19 @@
+package com.travisgray.foodchain;
+
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
